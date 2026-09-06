5d8b43c8570c48ede95585c53a31574f49c7843e
Raphael <raphael_albr@outlook.com>
2026-09-06 18:07:57 -0300

    fix: redirecionando rota inicial

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,17 @@
+import { Navigate } from "react-router-dom";
+
+import { useAuth } from "../hooks/useAuth";
+
 export function Home() {
-  return <h1 className="text-3xl">Home</h1>;
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <h1>Carregando...</h1>;
+  }
+
+  return user ? (
+    <Navigate to="/dashboard" replace />
+  ) : (
+    <Navigate to="/login" replace />
+  );
 }
